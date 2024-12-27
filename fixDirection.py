2d9d95cf02f9972d6038c0dd932e9b988a11e14b
Krishan <33421343+kfiven@users.noreply.github.com>
2024-12-27 20:36:56 +1100

    Update fixDirection.py

--- fixDirection.py
+++ fixDirection.py
@@ -1,19 +1,9 @@
-import os
+import fontforge
 import sys
-
-print("Executable:", sys.executable)
-print("Version:", sys.version)
-print("CWD:", os.getcwd())
-print("UName:", getattr(os, "uname", lambda: None)())
-for evn in ("PATH", "PYTHONHOME", "PYTHONPATH"):
-    print("{:s}: {:}".format(evn, os.environ.get(evn)))
-print("sys.path:", sys.path)
-
-import fontforge as ff
 
 fontfile = sys.argv[1]
 
-f = ff.open(fontfile)
+f = fontforge.open(fontfile)
 
 for glyph in f.glyphs():
     glyph.correctDirection()
